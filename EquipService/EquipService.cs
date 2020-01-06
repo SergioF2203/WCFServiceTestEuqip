7daea2108104551d806628cd10b6a1e57535f50a
SergioF <fs220381@gmail.com>
2020-01-06 12:38:57 +0200

    + Windows Service

---
+++ EquipService/EquipService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipService
+{
+    public partial class EquipService : ServiceBase
+    {
+        private ServiceHost service_host = null;
+        public EquipService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (service_host != null) service_host.Close();
+
+            string address_HTTP = "http://localhost:9000/EquipService";
+
+            Uri address_base = new Uri(address_HTTP);
+            service_host = new ServiceHost(typeof(WcfServiceTestEuqip.Service1), address_base);
+
+            ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+            service_host.Description.Behaviors.Add(behavior);
+
+            BasicHttpBinding binding_http = new BasicHttpBinding();
+            service_host.AddServiceEndpoint(typeof(WcfServiceTestEuqip.IServiceEuqip), binding_http, address_HTTP);
+            service_host.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "mex");
+
+            service_host.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (service_host != null)
+            {
+                service_host.Close();
+                service_host = null;
+            }
+        }
+    }
+}
---
+++ EquipService/EquipServiceInstaller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace EquipService
+{
+    [RunInstaller(true)]
+    public partial class EquipServiceInstaller : System.Configuration.Install.Installer
+    {
+        public EquipServiceInstaller()
+        {
+            //InitializeComponent();
+            serviceProcessInstaller1 = new ServiceProcessInstaller();
+            serviceProcessInstaller1.Account = ServiceAccount.LocalSystem;
+            serviceInstaller1 = new ServiceInstaller();
+            serviceInstaller1.ServiceName = "WindowsServiceHostForEquipService";
+            serviceInstaller1.DisplayName = "WindowsServiceHostForEquipService";
+            serviceInstaller1.Description = "WCF Service";
+            serviceInstaller1.StartType = ServiceStartMode.Automatic;
+            Installers.Add(serviceProcessInstaller1);
+            Installers.Add(serviceInstaller1);
+        }
+    }
+}
